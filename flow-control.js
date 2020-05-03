0e3a28642ae119ad2c7c9bef321e40e09652f5f2
Brett Wilson <wilson.3458@osu.edu>
2020-05-03 09:00:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,7 @@ function basicTeenager(age) {
 
 function teenager(age) {
   var result = "";
-  if(13 < age < 19){
+  if(13 < age && age < 19){
     result = "You are a teenager!";
   } else {
     result = "You are not a teenager";
@@ -27,7 +27,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return 13 < age < 19 ? "You are a teenager":"You are not a teenager";
+  return 13 < age && age < 19 ? "You are a teenager":"You are not a teenager";
 }
 
 function switchAge(age) {
